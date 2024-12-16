8df0b6312aa2b4731cf8e0714fe8f2cfa295fac4
ShaoXi <shaoxi9733@gmail.com>
2024-12-16 16:38:28 +0800

    Set up the menu & table booking API

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
+from rest_framework import viewsets
+from .models import Booking  # Import your Booking model
+from .serializers import BookingSerializer  # Import the BookingSerializer
 
-# Create your views here.
+# Regular Django view (for rendering an HTML page)
 def index(request):
     return render(request, 'index.html', {})
+
+# API view to list and create Menu items (GET and POST)
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# API view to retrieve, update, or delete a single Menu item (GET, PUT, DELETE)
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()  # Fetch all booking objects
+    serializer_class = BookingSerializer  # Use the BookingSerializer for this view
